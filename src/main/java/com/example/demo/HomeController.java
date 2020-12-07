910a4fdda8a0ab882922ae1a2fd10d0cf38c8635
eeeb0053 <eeeb0053@gmail.com>
2020-12-07 14:57:21 +0900

    20/12/07 15:00

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,14 +1,25 @@
 package com.example.demo;
 
+import javax.websocket.server.PathParam;
+
+import com.example.demo.utils.Printer;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
 public class HomeController {
+    @Autowired Printer printer;
     @GetMapping ("/")
     public String index(){
-        System.out.println("==== 서버가 시작됨 ====");
         return "index";
+    }
+    @GetMapping ("/move/{dir}/{page}")
+    public String move(@PathVariable String dir, @PathVariable String page){
+        printer.accept("이동경로: " +dir + "/" +page);
+        return String.format("%s/%s", dir, page);
     }
 }
